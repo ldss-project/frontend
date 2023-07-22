4db4d8f18637129bd2af63b1748d3d3259068a59
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-22 17:36:41 +0200

    component: add NavBar component to the application

--- src/view/components/MyComponent.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <p>This is my component.</p>
-</template>
-
-<style lang="scss" scoped>
-  p { color: $my-primary-color; }
-</style>
---
+++ src/view/components/NavBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import NavBarLink from "@/view/components/NavBarLink.vue";
+import NavBarLogo from "@/view/components/NavBarLogo.vue";
+
+// TODO Find if the user is a guest or is authenticated
+const isGuest = true
+
+const guestLinks = [
+  { to: { name: "log-in" }, text: "Log in" },
+  { to: { name: "leaderboard" }, text: "Leaderboard" },
+]
+
+const userLinks = [
+  { to: { name: "profile" }, text: "Profile" },
+  { to: { name: "statistics" }, text: "Statistics" },
+  { to: { name: "leaderboard" }, text: "Leaderboard" },
+  { to: { name: "log-out" }, text: "Logout" },
+]
+
+function links() { return isGuest ? guestLinks : userLinks }
+</script>
+
+<template>
+  <div class="nav-bar">
+    <NavBarLogo :to="{ name: 'homepage' }"/>
+    <NavBarLink v-for="link in links()" :to="link.to">{{ link.text }}</NavBarLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .nav-bar {
+    height: 100%;
+    border: $border-palette-dark;
+  }
+</style>
