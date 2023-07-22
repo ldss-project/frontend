3f3d124d29e53bfbf4398bcf21dda05fcee9ba80
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-22 17:37:17 +0200

    feat(pages): add nav bar to the pages of the application

--- src/view/App.vue
+++ src/view/App.vue
@@ -1,17 +1,43 @@
+<style lang="scss"> @import "src/assets/global"; </style>
+
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NavBar from "@/view/components/NavBar.vue";
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink class="btn btn-link" to="/">Homepage</RouterLink>
-      <RouterLink class="btn btn-link" :to="{ name: 'not-found' }">Not a page </RouterLink>
-    </nav>
-  </header>
-  <hr>
-
-  <RouterView />
+  <div class="application">
+    <header>
+      <nav>
+        <NavBar />
+      </nav>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .application {
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+    width: 100vw;
+  }
+  header {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    z-index: 9000;
+    width: $size-nav-bar;
+    background-color: $palette-dark-primary;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    padding-left: $size-nav-bar;
+    background-color: $palette-dark-secondary;
+  }
+</style>
