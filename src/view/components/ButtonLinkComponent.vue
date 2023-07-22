ae45ea6611a2c83c77a11311ae797f4456c69d65
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-23 00:46:51 +0200

    component: add ButtonComponent and ButtonLinkComponent

---
+++ src/view/components/ButtonLinkComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {RouterLink} from "vue-router";
+import ButtonComponent from "@/view/components/ButtonComponent.vue";
+defineProps(['to'])
+</script>
+
+<template>
+  <ButtonComponent>
+    <RouterLink :to="to">
+      <slot />
+    </RouterLink>
+  </ButtonComponent>
+</template>
+
+<style lang="scss" scoped>
+  a {
+    @extend .flex-row, .flex-center-content;
+    text-decoration: none;
+    color: $palette-dark-text-primary;
+  }
+</style>
