de6442b9d3d72fb1993b094f1568ae5c23ca7cab
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-22 17:35:27 +0200

    router: add placeholder routes to the pages of the application

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,15 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'homepage', component: Homepage },
+    { path: '/log-in', name: 'log-in', component: NotFoundPage },
+    { path: '/log-out', name: 'log-out', component: NotFoundPage },
+    { path: '/sign-in', name: 'sign-in', component: NotFoundPage },
+    { path: '/create-game', name: 'create-game', component: NotFoundPage },
+    { path: '/join-game', name: 'join-game', component: NotFoundPage },
+    { path: '/game', name: 'create-game', component: NotFoundPage },
+    { path: '/profile', name: 'profile', component: NotFoundPage },
+    { path: '/statistics', name: 'statistics', component: NotFoundPage },
+    { path: '/leaderboard', name: 'leaderboard', component: NotFoundPage },
     { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage },
   ]
 })
