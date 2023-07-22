f92719c6df5d59b5bcfc7316c3d738517abd843b
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-23 00:51:12 +0200

    feat(pages): add HomePage

--- src/view/pages/HomePage.vue
+++ src/view/pages/HomePage.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ButtonLinkComponent from "@/view/components/ButtonLinkComponent.vue";
+</script>
 
 <template>
-  <p>Home Page</p>
+  <ButtonLinkComponent :to="{ name: 'create-game' }">Create Game</ButtonLinkComponent>
+  <ButtonLinkComponent :to="{ name: 'join-game' }">Join Game</ButtonLinkComponent>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .button {
+    width: 30%;
+    margin: 5px;
+  }
+</style>
