4001bb2348ab0a21ea47884fc346ad8b9938f593
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-23 00:46:51 +0200

    component: add ButtonComponent and ButtonLinkComponent

---
+++ src/view/components/ButtonComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button" type="button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  .button {
+    @extend .flex-row, .flex-center-content;
+    height: $size-button;
+    border: $border-palette-dark;
+    border-radius: $size-border-radius;
+    background-color: $palette-dark-primary;
+    font-weight: bold;
+    color: $palette-dark-text-primary;
+  }
+  .button:hover {
+    cursor: pointer;
+    background-color: $palette-dark-primary-hover;
+  }
+  .button:active {
+    background-color: $palette-dark-secondary;
+  }
+</style>
