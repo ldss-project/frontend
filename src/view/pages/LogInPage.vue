085cac88e9fabeb9bd9140ea99a76591a546322e
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-23 22:08:24 +0200

    feat(pages): add LogInPage

--- src/view/pages/LogInPage.vue
+++ src/view/pages/LogInPage.vue
@@ -1,7 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FormComponent from "@/view/components/FormComponent.vue";
+import InputComponent from "@/view/components/InputComponent.vue";
+import ErrorText from "@/view/components/ErrorText.vue";
+import ButtonComponent from "@/view/components/ButtonComponent.vue";
+import ButtonLinkComponent from "@/view/components/ButtonLinkComponent.vue";
+import {ref} from "vue";
+
+const error = ref<{
+  input: "username" | "password",
+  message: string,
+}>()
+</script>
 
 <template>
-  <p>Log In Page</p>
+  <FormComponent>
+    <InputComponent
+      name="username"
+      type="text"
+      :valid="error?.input !== 'username'"
+      hidden-label
+      autocomplete="on"
+      required
+    />
+    <InputComponent
+      name="password"
+      type="password"
+      :valid="error?.input !== 'password'"
+      hidden-label
+      autocomplete="on"
+      required
+    />
+    <ErrorText
+      v-if="error"
+      :text="error.message"
+    />
+    <div class="form-actions">
+      <ButtonLinkComponent
+        class="button-tertiary"
+        :to="{ name: 'sign-in' }"
+      >Sign In</ButtonLinkComponent>
+      <ButtonComponent
+        class="button-primary"
+        type="submit"
+      >Log In</ButtonComponent>
+    </div>
+  </FormComponent>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .form-actions {
+    @extend .flex-row;
+    justify-content: space-between;
+    .button { width: 40%; }
+  }
+</style>
