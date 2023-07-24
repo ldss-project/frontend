9cabe535d8afca4dde542ed7988adafc8fbc5d5c
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-24 19:28:55 +0200

    fix(pages): fix barely visible text in NotFoundPage

--- src/view/pages/NotFoundPage.vue
+++ src/view/pages/NotFoundPage.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p>404: Page not found.</p>
+  <h1>404: Page not found.</h1>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  h1 {
+    height: fit-content;
+    width: fit-content;
+    margin: $size-margin;
+    padding: $size-padding;
+    font-weight: bold;
+    border: $border-palette-dark;
+    color: $palette-dark-text-tertiary;
+    background-color: $palette-dark-tertiary;
+  }
+</style>
