af1abc5c7afcce173a532f69f9743c663c5c9842
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-26 18:27:24 +0200

    feat(page): add LeaderboardPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,8 +21,8 @@ const router = createRouter({
     { path: '/create-game', name: 'create-game', component: CreateGamePage },
     { path: '/join-game', name: 'join-game', component: JoinGamePage },
     { path: '/game', name: 'game', component: GamePage },
-    { path: '/profile/:userId', name: 'profile', component: ProfilePage },
-    { path: '/statistics', name: 'statistics', component: StatisticsPage },
+    { path: '/profile/:username', name: 'profile', component: ProfilePage },
+    { path: '/statistics/:username', name: 'statistics', component: StatisticsPage },
     { path: '/leaderboard', name: 'leaderboard', component: LeaderboardPage },
     { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage },
   ]
