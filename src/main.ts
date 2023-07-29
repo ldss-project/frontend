b85ce25cad04f3a5fb4cc3cea59f1d0ff610c2b1
jahrim <jahrim.cesario2@studio.unibo.it>
2023-07-29 15:27:30 +0200

    build: add PrimeVue and Chart.js dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './view/App.vue'
+import PrimeVue from 'primevue/config';
+
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
